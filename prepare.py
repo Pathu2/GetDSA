cd1aac9b4e067b1cc334cb7243f059224b61ef78
Pathu2 <pathubhai@kgpian.iitkgp.ac.in>
2023-07-05 02:47:37 +0530

    Update app.py

--- prepare.py
+++ prepare.py
@@ -1,49 +1,67 @@
 
-# Read the file and get the lines
-with open("Qdata/index.txt", "r") as f:
-    # encoding nhi likha hi
-    lines = f.readlines()
-    # print(lines)
+import re
 
+# Read the file and get the lines
+with open("Qdata/index.txt", "r", encoding="cp1252") as f:
+    lines = f.readlines()
+
+question_data = [''] * 2035
+
+encodings = ["utf-8", "cp1252", "utf-16"]
+
+for index in range(0, 2034, 1):
+    encoded_index = str(index+1)
+    file_path = f"Qdata/{encoded_index}/{encoded_index}.txt"
+
+    for encoding in encodings:
+        try:
+            with open(file_path, "r", encoding=encoding) as f:
+                content = f.read()
+                match = re.search(r'\bExample\b', content)
+                if match:
+                    question_data[index] = content[:match.start()].lower().split()
+                else:
+                    question_data[index] = content.lower().split()
+                break
+        except UnicodeDecodeError:
+            continue
+
+print(len(lines))
+print(len(question_data))
 
 def preprocess(line):
     terms = [term.lower() for term in line.split()[1:]]
-    # .strip() ka use nhi hai toh nhi likh raha
     return terms
-
 
 documents = []
 vocab = {}
-# consider vocab like a json like, which has the key value pairs, key is the word and values
-# is how many times it being there in all the documents
-for line in lines:
+
+for index, line in enumerate(lines):
     tokens = preprocess(line)
-    documents.append(tokens)
+    question_lines = question_data[index]
+    combined_tokens = tokens + [line.strip() for line in question_lines]
+    documents.append(combined_tokens)
     for token in tokens:
         if token not in vocab:
             vocab[token] = 1
         else:
-            vocab[token] = vocab[token] + 1
+            vocab[token] += 1
 
-#resverse sorting the vocab
-vocab = dict(sorted(vocab.items(), key = lambda item: item[1], reverse=True ))
+# Reverse sorting the vocab
+vocab = dict(sorted(vocab.items(), key=lambda item: item[1], reverse=True))
 
-# saving the key and value in two different text files
-with open("vocab.txt", "w") as f:
+# Saving the key and value in two different text files
+with open("vocab.txt", "w", encoding="utf-8-sig") as f:
     for key in vocab.keys():
         f.write(key + '\n')
 
-with open("idf-values.txt", "w") as f:
+with open("idf-values.txt", "w", encoding="utf-8-sig") as f:
     for key in vocab.keys():
         f.write("%s\n" % vocab[key])
 
-
-with open("documents.txt", "w") as f:
+with open("documents.txt", "w", encoding="utf-8-sig") as f:
     for document in documents:
         f.write("%s\n" % ' '.join(document))
-#We have a document in documents as the array of wrods
-#to save them as a single line we had use ' '.join() 
-#which will join them 
 
 inverted_index = {}
 
@@ -51,12 +69,10 @@ for index, document in enumerate(documents):
     for token in document:
         if token not in inverted_index:
             inverted_index[token] = [index]
-            #if you normally write index instead of [] brackets, you will not able to append
-            #as it will be considered as single element
         else:
             inverted_index[token].append(index)
 
-with open("inverted_index.txt", "w") as f:
+with open("inverted_index.txt", "w", encoding="utf-8-sig") as f:
     for key in inverted_index.keys():
         f.write("%s\n" % key)
         f.write("%s\n" % ' '.join([str(doc_id) for doc_id in inverted_index[key]]))
